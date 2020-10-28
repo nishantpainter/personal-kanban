c9c3926d1ba59c1990a5e5a037695793fc74666c
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-10-28 19:27:04 +0530

    Add global decorators

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,25 @@
+import ThemeProvider from "../src/PersonalKanban/providers/ThemeProvider";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-}
+};
+
+const withThemeProvider = (Story, context) => {
+  const darkMode = context.globals.darkMode;
+  return (
+    <ThemeProvider darkMode={darkMode}>
+      <Story {...context} />
+    </ThemeProvider>
+  );
+};
+
+const withDivWrapper = (Story, context) => {
+  const story = <Story {...context} />;
+  return context.parameters.withWrapper ? (
+    <div style={{ width: 350 }}>{story}</div>
+  ) : (
+    story
+  );
+};
+
+export const decorators = [withThemeProvider, withDivWrapper];
