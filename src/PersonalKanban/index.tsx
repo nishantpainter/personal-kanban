cb049779f34a7505663c5fe64e0156960a6d12b3
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-10-28 19:27:48 +0530

    Configure theme provider

--- src/PersonalKanban/index.tsx
+++ src/PersonalKanban/index.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 
+import ThemeProvider from "PersonalKanban/providers/ThemeProvider";
+
 interface PersonalKanbanProps {}
 
 const PersonalKanban: React.FC<PersonalKanbanProps> = () => {
-  return <h3>Personal Kanban</h3>;
+  return (
+    <ThemeProvider>
+      <h3>Personal Kanban</h3>
+    </ThemeProvider>
+  );
 };
 
 export default PersonalKanban;
