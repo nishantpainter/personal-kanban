9b87a37f8da662310ffad8873c903abed09c743a
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-10-29 19:30:33 +0530

    Add card stories

---
+++ src/PersonalKanban/components/Card/Card.stories.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+import Card from "PersonalKanban/components/Card";
+
+const stories = {
+  title: "PersonalKanban/Card",
+  component: Card,
+};
+
+const Template = (args: any) => <Card {...args} />;
+
+export const Default: any = Template.bind({});
+Default.args = {
+  record: {
+    id: 1,
+    title: "Make Coffee",
+    description: "Brew vanilla coffee",
+    caption: "28 Oct 2020 5:40 PM",
+  },
+};
+Default.parameters = {
+  withWrapper: true,
+};
+
+export const LongTitle: any = Template.bind({});
+LongTitle.args = {
+  record: {
+    ...Default.args.record,
+    title: "Make Coffee with a very secretive recipe",
+  },
+};
+LongTitle.parameters = {
+  ...Default.parameters,
+};
+
+export const LongDescription: any = Template.bind({});
+LongDescription.args = {
+  record: {
+    ...Default.args.record,
+    description:
+      "With four ingredients and no special device or machine you can make one of the best tasting coffee at home with Nescafe. It takes also less than 15 minutes to make this awesome homemade instant coffee.",
+  },
+};
+LongDescription.parameters = {
+  ...Default.parameters,
+};
+
+export const LongCaption: any = Template.bind({});
+LongCaption.args = {
+  record: {
+    ...Default.args.record,
+    caption: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+};
+LongCaption.parameters = {
+  ...Default.parameters,
+};
+
+export default stories;
