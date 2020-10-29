396afb869dbcb5dd882f80a6de72498be1575c2d
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-10-29 19:30:27 +0530

    Add card

---
+++ src/PersonalKanban/components/Card/Card.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import clsx from "clsx";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+
+import { Record } from "PersonalKanban/types";
+
+const useStyles = makeStyles(() => ({
+  paper: {
+    height: 150,
+  },
+  description: {
+    maxHeight: "5rem",
+    minHeight: "5rem",
+    display: "-webkit-box",
+    "-webkit-line-clamp": 4,
+    "-webkit-box-orient": "vertical",
+    overflow: "hidden",
+  },
+}));
+
+type CardProps = {
+  record: Record;
+  className?: string;
+};
+
+const Card: React.FC<CardProps> = (props) => {
+  const { record, className } = props;
+  const { title, description, caption } = record;
+
+  const classes = useStyles();
+
+  return (
+    <Paper className={clsx(classes.paper, className)}>
+      <Typography title={title} gutterBottom noWrap>
+        <b>{title}</b>
+      </Typography>
+      <Typography
+        title={description}
+        className={classes.description}
+        variant="body2"
+        gutterBottom
+      >
+        {description}
+      </Typography>
+      <Typography component="p" variant="caption" noWrap>
+        {caption}
+      </Typography>
+    </Paper>
+  );
+};
+
+export default Card;
