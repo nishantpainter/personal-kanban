663c2ab2382ec9dc95f3f571e293a3bcf45711c3
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-10-29 19:31:15 +0530

    Add kanban board stories

---
+++ src/PersonalKanban/components/KanbanBoard/KanbanBoard.stories.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import faker from "faker";
+
+import KanbanBoard from "PersonalKanban/components/KanbanBoard";
+
+const stories = {
+  title: "PersonalKanban/KanbanBoard",
+  component: KanbanBoard,
+};
+
+const Template = (args: any) => <KanbanBoard {...args} />;
+
+export const Default: any = Template.bind({});
+Default.args = {
+  columns: [
+    {
+      id: 1,
+      title: "Todo",
+      description: "Task to be done today",
+      caption: "WIP Limits : 5",
+      records: [
+        {
+          id: 1,
+          title: "Make Coffee",
+          description: "Brew vanilla coffee",
+          caption: "28 Oct 2020 5:40 PM",
+        },
+      ],
+    },
+    {
+      id: 2,
+      title: "In-Progress",
+      description: "Task in progress today",
+      caption: "WIP Limits : 10",
+      records: [
+        {
+          id: 2,
+          title: "Brew Beans",
+          description: "Brew arabica coffee",
+          caption: "28 Oct 2020 5:40 PM",
+        },
+        {
+          id: 3,
+          title: "Bring Filters",
+          description:
+            "Cremica filter papers. Cone shaped white paper filter for Hario's V60 size 02 pour-over brewers. Contains 100 disposable white size 02 paper filters. These paper filters produce a clean, flavorful, sediment-free cup. Hario's paper filters make for convenient brewing and cleanup.",
+          caption: "27 Oct 2020 6:40 AM",
+        },
+      ],
+    },
+  ],
+};
+
+export const Stress: any = Template.bind({});
+Stress.args = {
+  columns: new Array(20).fill(0).map((_, id) => ({
+    id,
+    title: faker.commerce.department(),
+    description: faker.lorem.sentence(),
+    caption: faker.lorem.words(),
+    records: new Array(10).fill(0).map(() => ({
+      id: faker.random.uuid(),
+      title:
+        faker.commerce.productAdjective() + faker.commerce.productDescription(),
+      description: faker.lorem.sentences(),
+      caption: faker.lorem.words(),
+    })),
+  })),
+};
+
+export default stories;
