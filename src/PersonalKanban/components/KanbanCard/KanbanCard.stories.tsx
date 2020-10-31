05a8fcea65f950843b2ec5331016474545dde020
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-10-31 11:42:28 +0530

    Add kanban card stories

---
+++ src/PersonalKanban/components/KanbanCard/KanbanCard.stories.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
+
+import KanbanCard from "PersonalKanban/components/KanbanCard";
+
+const stories = {
+  title: "PersonalKanban/KanbanCard",
+  component: KanbanCard,
+};
+
+const Template = (args: any) => (
+  <DragDropContext onDragEnd={() => {}}>
+    <Droppable droppableId="BOARD" type="COLUMN" direction="horizontal">
+      {(provided) => {
+        return (
+          <div ref={provided.innerRef} {...provided.droppableProps}>
+            <KanbanCard {...args} />
+          </div>
+        );
+      }}
+    </Droppable>
+  </DragDropContext>
+);
+
+export const Default: any = Template.bind({});
+Default.args = {
+  record: {
+    id: 1,
+    title: "Make Coffee",
+    description: "Brew vanilla coffee",
+    caption: "28 Oct 2020 5:40 PM",
+  },
+};
+Default.parameters = {
+  withWrapper: true,
+};
+
+export default stories;
