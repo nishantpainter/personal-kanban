f36f74a17e1a686e6c3125091fe1765b81630af1
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-11-01 12:18:54 +0530

    Add radio

---
+++ src/PersonalKanban/components/Radio/Radio.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import MuiRadio, { RadioProps as MuiRadioProps } from "@material-ui/core/Radio";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(() => ({
+  root: {
+    color: (props: any) => props.color,
+    "&$checked": {
+      color: (props: any) => props.color,
+    },
+  },
+}));
+
+type RadioProps = Omit<MuiRadioProps, "color"> & { color?: string };
+
+const Radio = React.forwardRef((props: RadioProps, ref: any) => {
+  const { color, ...rest } = props;
+
+  const classes = useStyles({ color });
+
+  return (
+    <MuiRadio
+      ref={ref}
+      color="default"
+      classes={color ? classes : {}}
+      {...rest}
+    />
+  );
+});
+
+export default Radio;
