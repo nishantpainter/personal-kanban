bbfadc07833e49254db8775d8a3b0b4e44b67c65
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-11-01 11:18:59 +0530

    Add record form stories

---
+++ src/PersonalKanban/components/RecordForm/RecordForm.stories.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import RecordForm from "PersonalKanban/components/RecordForm";
+
+const stories = {
+  title: "PersonalKanban/RecordForm",
+  component: RecordForm,
+};
+
+const Template = (args: any) => <RecordForm {...args} />;
+
+export const Default: any = Template.bind({});
+Default.args = {
+  record: {
+    title: "Make Coffee",
+    description: "Brew the beans",
+  },
+};
+Default.parameters = {
+  withWrapper: true,
+};
+
+export const Disabled: any = Template.bind({});
+Disabled.args = {
+  ...Default.args,
+  disabled: true,
+};
+Disabled.parameters = {
+  ...Default.parameters,
+};
+
+export default stories;
