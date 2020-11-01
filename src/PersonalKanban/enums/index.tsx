2640b2ca49e86bed168f1667ec2858f408b4e445
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-11-01 12:19:11 +0530

    Add record color enum

---
+++ src/PersonalKanban/enums/index.tsx
@@ -0,0 +1,19 @@
+import purple from "@material-ui/core/colors/purple";
+import indigo from "@material-ui/core/colors/indigo";
+import blue from "@material-ui/core/colors/blue";
+import green from "@material-ui/core/colors/green";
+import yellow from "@material-ui/core/colors/yellow";
+import orange from "@material-ui/core/colors/orange";
+import red from "@material-ui/core/colors/red";
+
+const SHADE = 200;
+
+export const RecordColor = {
+  Purple: purple[SHADE],
+  Indigo: indigo[SHADE],
+  Blue: blue[SHADE],
+  Green: green[SHADE],
+  Yellow: yellow[SHADE],
+  Orange: orange[SHADE],
+  Red: red[SHADE],
+};
