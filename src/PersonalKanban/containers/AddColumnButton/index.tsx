6aebb002ea5c59cfaac82e1a7d3cf87436dc4255
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-11-06 09:38:07 +0530

    Update with column form

--- src/PersonalKanban/containers/AddColumnButton/index.tsx
+++ src/PersonalKanban/containers/AddColumnButton/index.tsx
@@ -1,15 +1,40 @@
 import React from "react";
 import Box from "@material-ui/core/Box";
 import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogContent from "@material-ui/core/DialogContent";
+
+import ColumnForm from "PersonalKanban/components/ColumnForm";
 
 type AddColumnButtonProps = {};
 
 const AddColumnButton: React.FC<AddColumnButtonProps> = (props) => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpenDialog = React.useCallback(() => {
+    setOpen(true);
+  }, []);
+
+  const handleCloseDialog = React.useCallback(() => {
+    setOpen(false);
+  }, []);
+
+  const handleSubmit = React.useCallback(() => {}, []);
+
   return (
     <Box display="block" marginBottom={2}>
-      <Button variant="contained" color="primary">
+      <Button variant="contained" color="primary" onClick={handleOpenDialog}>
         Add Column
       </Button>
+      <Dialog onClose={handleCloseDialog} open={open}>
+        <DialogContent>
+          <ColumnForm
+            column={{}}
+            onSubmit={handleSubmit}
+            onCancel={handleCloseDialog}
+          />
+        </DialogContent>
+      </Dialog>
     </Box>
   );
 };
