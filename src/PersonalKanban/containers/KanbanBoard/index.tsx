81a37bd0ca6818d23f17261de2800619b5fa137d
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-11-07 12:13:00 +0530

    Handle new column

--- src/PersonalKanban/containers/KanbanBoard/index.tsx
+++ src/PersonalKanban/containers/KanbanBoard/index.tsx
@@ -1,17 +1,30 @@
 import React from "react";
-import Box from "@material-ui/core/Box";  
+import { v4 as uuidv4 } from "uuid";
+import Box from "@material-ui/core/Box";
 
 import AddColumnButton from "PersonalKanban/containers/AddColumnButton";
 import KanbanBoard from "PersonalKanban/components/KanbanBoard";
+import { Column } from "PersonalKanban/types";
+
+const getId = (): string => {
+  return uuidv4();
+};
 
 type KanbanBoardContainerProps = {};
 
 const KanbanBoardContainer: React.FC<KanbanBoardContainerProps> = (props) => {
-  const [columns, setColumns] = React.useState([]);
+  const [columns, setColumns] = React.useState<Column[]>([]);
+
+  const handleAddColumn = React.useCallback((column: Column) => {
+    setColumns((columns: Column[]) => [
+      ...columns,
+      Object.assign({ id: getId() }, column),
+    ]);
+  }, []);
 
   return (
     <Box padding={1}>
-      <AddColumnButton />
+      <AddColumnButton onSubmit={handleAddColumn} />
       <KanbanBoard columns={columns} />
     </Box>
   );
