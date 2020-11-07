86793640d768da3fe68dca27a67acb3793314687
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-11-07 12:22:17 +0530

    Handle column move

--- src/PersonalKanban/containers/KanbanBoard/index.tsx
+++ src/PersonalKanban/containers/KanbanBoard/index.tsx
@@ -1,19 +1,22 @@
 import React from "react";
-import { v4 as uuidv4 } from "uuid";
 import Box from "@material-ui/core/Box";
 
 import AddColumnButton from "PersonalKanban/containers/AddColumnButton";
 import KanbanBoard from "PersonalKanban/components/KanbanBoard";
 import { Column } from "PersonalKanban/types";
-
-const getId = (): string => {
-  return uuidv4();
-};
+import { getId, reorder } from "PersonalKanban/services/Utils";
 
 type KanbanBoardContainerProps = {};
 
 const KanbanBoardContainer: React.FC<KanbanBoardContainerProps> = (props) => {
   const [columns, setColumns] = React.useState<Column[]>([]);
+
+  const getColumnIndex = React.useCallback(
+    (id: string) => {
+      return columns.findIndex((c: Column) => c.id === id);
+    },
+    [columns]
+  );
 
   const handleAddColumn = React.useCallback((column: Column) => {
     setColumns((columns: Column[]) => [
@@ -22,10 +25,18 @@ const KanbanBoardContainer: React.FC<KanbanBoardContainerProps> = (props) => {
     ]);
   }, []);
 
+  const handleColumnMove = React.useCallback(
+    ({ column, index }: { column: Column; index: number }) => {
+      const updatedColumns = reorder(columns, getColumnIndex(column.id), index);
+      setColumns(updatedColumns);
+    },
+    [columns, getColumnIndex]
+  );
+
   return (
     <Box padding={1}>
       <AddColumnButton onSubmit={handleAddColumn} />
-      <KanbanBoard columns={columns} />
+      <KanbanBoard columns={columns} onColumnMove={handleColumnMove} />
     </Box>
   );
 };
