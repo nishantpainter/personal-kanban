5b0fb093a7ecbee11fa4941bdbdbe80cf4e7c6b4
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-11-07 13:10:43 +0530

    Add no record display message

--- src/PersonalKanban/components/Column/Column.tsx
+++ src/PersonalKanban/components/Column/Column.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
@@ -94,16 +93,18 @@ export const ColumnCardList: React.FC<ColumnCardListProps> = (props) => {
 
   return (
     <div ref={innerRef}>
-      {records && records.length
-        ? records.map((record: Record, index) => (
-            <CardComponent
-              key={record.id}
-              record={record}
-              className={classes.card}
-              index={index}
-            />
-          ))
-        : null}
+      {records && records.length ? (
+        records.map((record: Record, index) => (
+          <CardComponent
+            key={record.id}
+            record={record}
+            className={classes.card}
+            index={index}
+          />
+        ))
+      ) : (
+        <Typography>No records available.</Typography>
+      )}
     </div>
   );
 };
