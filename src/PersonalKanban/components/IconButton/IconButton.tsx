6dc3e34c1c6e6b178d9f2847b334c032b94b2b06
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-11-07 12:46:52 +0530

    Add icon button

---
+++ src/PersonalKanban/components/IconButton/IconButton.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import MuiIconButton, {
+  IconButtonProps as MuiIconButtonProps,
+} from "@material-ui/core/IconButton";
+
+import DeleteIcon from "@material-ui/icons/Delete";
+import EditIcon from "@material-ui/icons/Edit";
+
+const icons = {
+  edit: EditIcon,
+  delete: DeleteIcon,
+};
+
+type IconButtonProps = MuiIconButtonProps & { icon: keyof typeof icons };
+
+const IconButton: React.FC<IconButtonProps> = (props) => {
+  const { icon, ...rest } = props;
+  const Icon = icons[icon];
+  return Icon ? (
+    <MuiIconButton size="small" {...rest}>
+      <Icon />
+    </MuiIconButton>
+  ) : null;
+};
+
+export default IconButton;
