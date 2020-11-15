32821f3c775c020059e942ab3d20345329bb872a
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-11-15 17:53:30 +0530

    Add column shade

--- src/PersonalKanban/enums/index.tsx
+++ src/PersonalKanban/enums/index.tsx
@@ -6,15 +6,28 @@ import yellow from "@material-ui/core/colors/yellow";
 import orange from "@material-ui/core/colors/orange";
 import red from "@material-ui/core/colors/red";
 
-const SHADE = 200;
+const RECORD_SHADE = 200;
 
 export const RecordColor = {
-  Purple: purple[SHADE],
-  Indigo: indigo[SHADE],
-  Blue: blue[SHADE],
-  Green: green[SHADE],
-  Yellow: yellow[SHADE],
-  Orange: orange[SHADE],
-  Red: red[SHADE],
+  Purple: purple[RECORD_SHADE],
+  Indigo: indigo[RECORD_SHADE],
+  Blue: blue[RECORD_SHADE],
+  Green: green[RECORD_SHADE],
+  Yellow: yellow[RECORD_SHADE],
+  Orange: orange[RECORD_SHADE],
+  Red: red[RECORD_SHADE],
+  Default: "",
+};
+
+const COLUMN_SHADE = 100;
+
+export const ColumnColor = {
+  Purple: purple[COLUMN_SHADE],
+  Indigo: indigo[COLUMN_SHADE],
+  Blue: blue[COLUMN_SHADE],
+  Green: green[COLUMN_SHADE],
+  Yellow: yellow[COLUMN_SHADE],
+  Orange: orange[COLUMN_SHADE],
+  Red: red[COLUMN_SHADE],
   Default: "",
 };
