c3042145c20d0922e993eca4a3cc7de455c6d0d3
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-11-15 18:30:57 +0530

    Add cloumn color radio in form

--- src/PersonalKanban/components/ColumnForm/ColumnForm.tsx
+++ src/PersonalKanban/components/ColumnForm/ColumnForm.tsx
@@ -5,8 +5,13 @@ import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
 import Divider from "@material-ui/core/Divider";
 import Typography from "@material-ui/core/Typography";
+import FormControl from "@material-ui/core/FormControl";
+import FormLabel from "@material-ui/core/FormLabel";
+import RadioGroup from "@material-ui/core/RadioGroup";
 
 import { Column } from "PersonalKanban/types";
+import { ColumnColor } from "PersonalKanban/enums/index";
+import Radio from "PersonalKanban/components/Radio";
 
 type ColumnFormProps = {
   column?: Partial<Column>;
@@ -24,6 +29,7 @@ const ColumnForm: React.FC<ColumnFormProps> = (props) => {
       {
         title: "",
         description: "",
+        color: "",
       },
       column
     ),
@@ -71,6 +77,29 @@ const ColumnForm: React.FC<ColumnFormProps> = (props) => {
             disabled={disabled}
             onChange={handleChange}
           />
+        </Grid>
+        <Grid item xs={12}>
+          <FormControl component="fieldset">
+            <FormLabel component="legend">Background</FormLabel>
+            <RadioGroup
+              row
+              aria-label="background"
+              name="color"
+              value={values.color}
+              onChange={handleChange}
+            >
+              {Object.keys(ColumnColor).map((key) => {
+                const colorKey = key as keyof typeof ColumnColor;
+                return (
+                  <Radio
+                    key={colorKey}
+                    value={colorKey}
+                    color={ColumnColor[colorKey]}
+                  />
+                );
+              })}
+            </RadioGroup>
+          </FormControl>
         </Grid>
         <Grid item xs={12}>
           <Button variant="outlined" disabled={disabled} onClick={onCancel}>
