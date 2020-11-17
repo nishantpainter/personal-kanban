8f26c6d01fd0fb2519731cf415142de1e8de097e
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-11-17 15:59:05 +0530

    Configure translation provider

--- src/PersonalKanban/index.tsx
+++ src/PersonalKanban/index.tsx
@@ -2,13 +2,16 @@ import React from "react";
 
 import ThemeProvider from "PersonalKanban/providers/ThemeProvider";
 import KanbanBoardContainer from "PersonalKanban/containers/KanbanBoard";
+import TranslationProvider from "./providers/TranslationProvider";
 
 interface PersonalKanbanProps {}
 
 const PersonalKanban: React.FC<PersonalKanbanProps> = () => {
   return (
     <ThemeProvider>
-      <KanbanBoardContainer />
+      <TranslationProvider>
+        <KanbanBoardContainer />
+      </TranslationProvider>
     </ThemeProvider>
   );
 };
